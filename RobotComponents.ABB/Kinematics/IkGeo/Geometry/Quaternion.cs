5c784daad397c81988c9b1c8a3d662cb3fbe1e0e
Jan Philipp Drude <janphilipp.drude@asl.uni-kassel.de>
2025-11-05 12:37:25 +0100

    Add IK solver for CRB15000 robots and supporting structs
    Introduced an inverse kinematics solver (`IkGeoSolver`) for CRB15000 (GoFa) robots, wrapping the native `ik-geo` library. Added binary dependencies (`ikgeoInterface_GoFa.dll`, `libgcc_s_seh-1.dll`, `libstdc++-6.dll`, `libwinpthread-1.dll`) required for the solver.
    
    Added supporting geometry structs:
    - `Quaternion`: Represents quaternions with conversion methods.
    - `Vector3d`: Represents 3D vectors with Rhino type conversions.
    - `Vector6d`: Represents 6D robot joint positions with utility methods.
    
    Implemented `Compute_CRB15000` to calculate IK solutions, handle singularities, and convert results to robot configurations. Added detailed documentation for all new components.

---
+++ RobotComponents.ABB/Kinematics/IkGeo/Geometry/Quaternion.cs
@@ -0,0 +1,69 @@
+﻿// This file is part of Robot Components. Robot Components is licensed 
+// under the terms of GNU General Public License version 3.0 (GPL v3.0)
+// as published by the Free Software Foundation. For more information and 
+// the LICENSE file, see <https://github.com/RobotComponents/RobotComponents>.
+
+namespace RobotComponents.ABB.Kinematics.IkGeo.Geometry
+{
+    /// <summary>
+    /// Represents IkGeo's internal Quaternion class.
+    /// </summary>
+    /// <remarks>
+    /// Be careful with quaternion naming conventions. 
+    /// The conversion is as follows; A=w, B=x, C=y, D=z.
+    /// </remarks>
+    internal struct Quaternion
+    {
+        /// <summary>
+        /// Public components of the quaternion.
+        /// </summary>
+        /// <remarks>
+        /// Fields are named according to IkGeo's convention.
+        /// </remarks>
+        public double x, y, z, w;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Quaternion"/> structure with the specified components.
+        /// </summary>
+        /// <param name="x">The X component of the quaternion. (<c>B</c> component of Rhino quaternion)</param>
+        /// <param name="y">The Y component of the quaternion. (<c>C</c> component of Rhino quaternion)</param>
+        /// <param name="z">The Z component of the quaternion. (<c>D</c> component of Rhino quaternion)</param>
+        /// <param name="w">The W component of the quaternion, typically representing the scalar part. (<c>A</c> component of Rhino quaternion)</param>
+        public Quaternion(double x, double y, double z, double w)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Quaternion"/> structure using the specified 
+        /// <see cref="Rhino.Geometry.Quaternion"/>.
+        /// </summary>
+        /// <remarks>The components of the <see cref="Quaternion"/> are mapped as follows: 
+        /// <list type="bullet"> <item><description><c>x</c> is set to the <c>B</c> component of the input
+        /// quaternion.</description></item> <item><description><c>y</c> is set to the <c>C</c> component of the input
+        /// quaternion.</description></item> <item><description><c>z</c> is set to the <c>D</c> component of the input
+        /// quaternion.</description></item> <item><description><c>w</c> is set to the <c>A</c> component of the input
+        /// quaternion.</description></item> </list></remarks>
+        /// <param name="quaternion">The <see cref="Rhino.Geometry.Quaternion"/> instance from which to initialize the components of the
+        /// quaternion.</param>
+        public Quaternion(Rhino.Geometry.Quaternion quaternion)
+        {
+            x = quaternion.B;
+            y = quaternion.C;
+            z = quaternion.D;
+            w = quaternion.A;
+        }
+
+        /// <summary>
+        /// Converts this <see cref="Quaternion"/> instance to a <see cref="Rhino.Geometry.Quaternion"/>.
+        /// </summary>
+        public Rhino.Geometry.Quaternion ToRhinoQuaternion()
+        {
+            return new Rhino.Geometry.Quaternion(w, x, y, z);
+        }
+    }
+}
+
