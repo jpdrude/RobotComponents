39531f5738cbe6af0135248070e0025f57c9c1db
Jan Philipp Drude <jpdrude@gmail.com>
2026-02-10 08:34:30 +0100

    Add RAPID WaitRob instruction and GH component support
    - Implement WaitRob class for RAPID WaitRob instruction (InPos/ZeroSpeed)
    - Add GH_WaitRob Goo, WaitRobComponent, and Param_WaitRob
    - Register new icons and update resources for WaitRob

---
+++ RobotComponents.ABB.Gh/Components/Code Generation/Instructions/WaitRobComponent.cs
@@ -0,0 +1,113 @@
+﻿// SPDX-License-Identifier: GPL-3.0-or-later
+// This file is part of Robot Components (Modified)
+// Original project: https://github.com/RobotComponents/RobotComponents
+// Modified project: https://github.com/jpdrude/RobotComponents
+//
+// Copyright (c) 2026 EDEK Uni Kassel
+//
+// Author:
+//   - Jan Philipp Drude (2026)
+//
+// For license details, see the LICENSE file in the project root.
+
+// System Libs
+using System;
+// Grasshopper Libs
+using Grasshopper.Kernel;
+// RobotComponents Libs
+using RobotComponents.ABB.Actions.Instructions;
+using RobotComponents.ABB.Gh.Parameters.Actions.Instructions;
+
+namespace RobotComponents.ABB.Gh.Components.CodeGeneration
+{
+    /// <summary>
+    /// RobotComponents Action : WaitRob component.
+    /// </summary>
+    public class WaitRobComponent : GH_RobotComponent
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, Subcategory the panel. 
+        /// If you use non-existing tab or panel names, new tabs/panels will automatically be created.
+        /// </summary>
+        public WaitRobComponent() : base("Wait for Robot", "WR", "Code Generation",
+              "Defines an instruction to wait for a mechanical unit to finish its movement before proceeding with the next RAPID instruction.")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("Zero Speed", "ZS", "Specifies whether or not the mechanial units must have come to a full standstill before execution continues.", GH_ParamAccess.item, false);
+            pManager[0].Optional = true;
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.RegisterParam(new Param_WaitRob(), "Wait Robot", "WR", "Resulting Wait Rob instruction");
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // Input variables
+            bool zeroSpeed = false;
+
+            // Catch the input variables
+            if (!DA.GetData(0, ref zeroSpeed)) { zeroSpeed = false; }
+
+            // Create the action
+            WaitRob waitRob = new WaitRob(zeroSpeed);
+
+            // Output
+            DA.SetData(0, waitRob);
+        }
+
+        #region properties
+        /// <summary>
+        /// Override the component exposure (makes the tab subcategory).
+        /// Can be set to hidden, primary, secondary, tertiary, quarternary, quinary, senary, septenary and obscure
+        /// </summary>
+        public override GH_Exposure Exposure
+        {
+            get { return GH_Exposure.secondary; }
+        }
+
+        /// <summary>
+        /// Gets whether this object is obsolete.
+        /// </summary>
+        public override bool Obsolete
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get { return Properties.Resources.WaitRob_Icon; }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("67D62BDC-44AE-425D-A921-48218D180686"); }
+        }
+        #endregion
+    }
+}
---
+++ RobotComponents.ABB.Gh/Parameters/Actions/Instructions/Param_WaitRob.cs
@@ -0,0 +1,75 @@
+﻿// SPDX-License-Identifier: GPL-3.0-or-later
+// This file is part of Robot Components (Modified)
+// Original project: https://github.com/RobotComponents/RobotComponents
+// Modified project: https://github.com/jpdrude/RobotComponents
+//
+// Copyright (c) 2026 EDEK Uni Kassel
+//
+// Author:
+//   - Jan Philipp Drude (2026)
+//
+// For license details, see the LICENSE file in the project root.
+
+// System Libs
+using System;
+// Grasshopper Libs
+using Grasshopper.Kernel;
+// RobotComponents Libs
+using RobotComponents.ABB.Gh.Goos.Actions.Instructions;
+
+namespace RobotComponents.ABB.Gh.Parameters.Actions.Instructions
+{
+    /// <summary>
+    /// WaitRob parameter
+    /// </summary>
+    public class Param_WaitRob : GH_RobotParam<GH_WaitRob>
+    {
+        /// <summary>
+        /// Initializes a new instance of the Param_WaitRob class
+        /// </summary>
+        public Param_WaitRob() : base("Wait for Robot Parameter", "WR", "Parameters",
+                "Contains the data of a Wait for Robot instruction.")
+        {
+        }
+
+        /// <summary>
+        /// Converts this structure to a human-readable string.
+        /// </summary>
+        /// <returns> A string representation of the parameter. </returns>
+        public override string ToString()
+        {
+            return "Wait for robot";
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the object. This field typically remains fixed during the lifetime of an object.
+        /// </summary>
+        public override string Name { get => "Wait for robot"; set => base.Name = value; }
+
+        /// <summary>
+        /// Override this function to supply a custom icon (24x24 pixels). 
+        /// The result of this property is cached, so don't worry if icon retrieval is not very fast.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get { return Properties.Resources.WaitRob_Parameter_Icon; }
+        }
+
+        /// <summary>
+        /// Gets the exposure of this object in the Graphical User Interface.The default is to expose everywhere.
+        /// </summary>
+        public override GH_Exposure Exposure
+        {
+            get { return GH_Exposure.tertiary; }
+        }
+
+        /// <summary>
+        /// Returns a consistent ID for this object type. 
+        /// Every object must supply a unique and unchanging ID that is used to identify objects of the same type.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("9B632A51-5011-468D-B978-31F7890B1F52"); }
+        }
+    }
+}
