4a394e55dcd17c961995aa1199b6ebb9bb8f133f
Jan Philipp Drude <jpdrude@gmail.com>
2026-02-10 08:34:30 +0100

    Add RAPID WaitRob instruction and GH component support
    - Implement WaitRob class for RAPID WaitRob instruction (InPos/ZeroSpeed)
    - Add GH_WaitRob Goo, WaitRobComponent, and Param_WaitRob
    - Register new icons and update resources for WaitRob

---
+++ RobotComponents.ABB.Gh.Goos/Actions/Instructions/GH_WaitRob.cs
@@ -0,0 +1,298 @@
+﻿// SPDX-License-Identifier: GPL-3.0-or-later
+// This file is part of Robot Components (Modified)
+// Original project: https://github.com/RobotComponents/RobotComponents
+// Modified project: https://github.com/jpdrude/RobotComponents
+//
+// Copyright (c) 2026 EDEK Uni Kassel
+//
+// Author:
+//   - Jan Philipp Drude (2026)
+//
+// For license details, see the LICENSE file in the project root.
+
+// Grasshopper Libs
+using Grasshopper.Kernel.Types;
+using GH_IO;
+using GH_IO.Serialization;
+// RobotComponents Libs
+using RobotComponents.ABB.Actions;
+using RobotComponents.ABB.Actions.Instructions;
+
+namespace RobotComponents.ABB.Gh.Goos.Actions.Instructions
+{
+    /// <summary>
+    /// Wait Rob Goo wrapper class, makes sure the Wait Rob class can be used in Grasshopper.
+    /// </summary>
+    public class GH_WaitRob : GH_Goo<WaitRob>, GH_ISerializable
+    {
+        #region constructors
+        /// <summary>
+        /// Blank constructor
+        /// </summary>
+        public GH_WaitRob()
+        {
+            this.Value = null;
+        }
+
+        /// <summary>
+        /// Data constructor: Creates a Wait Rob Goo instance from a Wait Rob instance.
+        /// </summary>
+        /// <param name="waitRob"> WaitRob Value to store inside this Goo instance. </param>
+        public GH_WaitRob(WaitRob waitRob)
+        {
+            this.Value = waitRob;
+        }
+
+        /// <summary>
+        /// Data constructor: Creates a Wait Rob Goo instance from another Wait Rob Goo instance.
+        /// This creates a shallow copy of the passed Wait Rob Goo instance. 
+        /// </summary>
+        /// <param name="waitRobGoo"> Wait Rob Goo instacne to copy. </param>
+        public GH_WaitRob(GH_WaitRob waitRobGoo)
+        {
+            if (waitRobGoo == null)
+            {
+                waitRobGoo = new GH_WaitRob();
+            }
+
+            this.Value = waitRobGoo.Value;
+        }
+
+        /// <summary>
+        /// Make a complete duplicate of this Goo instance. No shallow copies.
+        /// </summary>
+        /// <returns> A duplicate of the WaitRobGoo. </returns>
+        public override IGH_Goo Duplicate()
+        {
+            return new GH_WaitRob(Value == null ? new WaitRob() : Value.Duplicate());
+        }
+        #endregion
+
+        #region properties
+        /// <summary>
+        /// Gets a value indicating whether or not the current value is valid.
+        /// </summary>
+        public override bool IsValid
+        {
+            get
+            {
+                if (Value == null) { return false; }
+                return Value.IsValid;
+            }
+        }
+
+        /// <summary>
+        /// Gets a string describing the state of "invalidness". 
+        /// If the instance is valid, then this property should return Nothing or string.Empty.
+        /// </summary>
+        public override string IsValidWhyNot
+        {
+            get
+            {
+                if (Value == null) { return "No internal Wait Rob instance"; }
+                if (Value.IsValid) { return string.Empty; }
+                return "Invalid Wait Rob instance: Did you define a duration?";
+            }
+        }
+
+        /// <summary>
+        /// Creates a string description of the current instance value
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            if (Value == null) { return "Null Wait Rob"; }
+            else { return Value.ToString(); }
+        }
+
+        /// <summary>
+        /// Gets the name of the type of the implementation.
+        /// </summary>
+        public override string TypeName
+        {
+            get { return "Wait Rob"; }
+        }
+
+        /// <summary>
+        /// Gets a description of the type of the implementation.
+        /// </summary>
+        public override string TypeDescription
+        {
+            get { return "Defines a Wait Rob."; }
+        }
+        #endregion
+
+        #region casting methods
+        /// <summary>
+        /// Attempt a cast to type Q.
+        /// </summary>
+        /// <typeparam name="Q"> Type to cast to.  </typeparam>
+        /// <param name="target"> Pointer to target of cast. </param>
+        /// <returns> True on success, false on failure. </returns>
+        public override bool CastTo<Q>(ref Q target)
+        {
+            //Cast to Wait Rob Goo
+            if (typeof(Q).IsAssignableFrom(typeof(GH_WaitRob)))
+            {
+                if (Value == null) { target = (Q)(object)new GH_WaitRob(); }
+                else { target = (Q)(object)new GH_WaitRob(Value); }
+                return true;
+            }
+
+            //Cast to Wait Time
+            if (typeof(Q).IsAssignableFrom(typeof(WaitRob)))
+            {
+                if (Value == null) { target = (Q)(object)null; }
+                else { target = (Q)(object)Value; }
+                return true;
+            }
+
+            //Cast to Action Goo
+            if (typeof(Q).IsAssignableFrom(typeof(GH_Action)))
+            {
+                if (Value == null) { target = (Q)(object)new GH_Action(); }
+                else { target = (Q)(object)new GH_Action(Value); }
+                return true;
+            }
+
+            //Cast to Action
+            if (typeof(Q).IsAssignableFrom(typeof(IAction)))
+            {
+                if (Value == null) { target = (Q)(object)null; }
+                else { target = (Q)(object)Value; }
+                return true;
+            }
+
+            //Cast to Instruction Goo
+            if (typeof(Q).IsAssignableFrom(typeof(GH_Instruction)))
+            {
+                if (Value == null) { target = (Q)(object)new GH_Instruction(); }
+                else { target = (Q)(object)new GH_Instruction(Value); }
+                return true;
+            }
+
+            //Cast to Instruction
+            if (typeof(Q).IsAssignableFrom(typeof(IInstruction)))
+            {
+                if (Value == null) { target = (Q)(object)null; }
+                else { target = (Q)(object)Value; }
+                return true;
+            }
+
+            //Cast to Bool
+            if (typeof(Q).IsAssignableFrom(typeof(GH_Boolean)))
+            {
+                if (Value == null) { target = default; }
+                else { target = (Q)(object)new GH_Boolean(Value.InPosition); }
+                return true;
+            }
+
+            target = default;
+            return false;
+        }
+
+        /// <summary>
+        /// Attempt a cast from generic object.
+        /// </summary>
+        /// <param name="source"> Reference to source of cast. </param>
+        /// <returns> True on success, false on failure. </returns>
+        public override bool CastFrom(object source)
+        {
+            if (source == null) { return false; }
+
+            //Cast from Wait Time
+            if (typeof(WaitTime).IsAssignableFrom(source.GetType()))
+            {
+                Value = source as WaitRob;
+                return true;
+            }
+
+            //Cast from Action
+            if (typeof(IAction).IsAssignableFrom(source.GetType()))
+            {
+                if (source is WaitRob action)
+                {
+                    Value = action;
+                    return true;
+                }
+            }
+
+            //Cast from Action Goo
+            if (typeof(GH_Action).IsAssignableFrom(source.GetType()))
+            {
+                GH_Action actionGoo = source as GH_Action;
+                if (actionGoo.Value is WaitRob action)
+                {
+                    Value = action;
+                    return true;
+                }
+            }
+
+            //Cast from Instruction
+            if (typeof(IInstruction).IsAssignableFrom(source.GetType()))
+            {
+                if (source is WaitRob instruction)
+                {
+                    Value = instruction;
+                    return true;
+                }
+            }
+
+            //Cast from Instruction Goo
+            if (typeof(GH_Instruction).IsAssignableFrom(source.GetType()))
+            {
+                GH_Instruction instructionGoo = source as GH_Instruction;
+                if (instructionGoo.Value is WaitRob instruction)
+                {
+                    Value = instruction;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        #endregion
+
+        #region (de)serialisation
+        /// <summary>
+        /// IO key for (de)serialisation of the value inside this Goo.
+        /// </summary>
+        private const string IoKey = "Wait Rob";
+
+        /// <summary>
+        /// This method is called whenever the instance is required to serialize itself.
+        /// </summary>
+        /// <param name="writer"> Writer object to serialize with. </param>
+        /// <returns> True on success, false on failure. </returns>
+        public override bool Write(GH_IWriter writer)
+        {
+            if (this.Value != null)
+            {
+                byte[] array = RobotComponents.Utils.Serialization.ObjectToByteArray(this.Value);
+                writer.SetByteArray(IoKey, array);
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// This method is called whenever the instance is required to deserialize itself.
+        /// </summary>
+        /// <param name="reader"> Reader object to deserialize from. </param>
+        /// <returns> True on success, false on failure. </returns>
+        public override bool Read(GH_IReader reader)
+        {
+            if (!reader.ItemExists(IoKey))
+            {
+                this.Value = null;
+                return true;
+            }
+
+            byte[] array = reader.GetByteArray(IoKey);
+            this.Value = (WaitRob)RobotComponents.Utils.Serialization.ByteArrayToObject(array);
+
+            return true;
+        }
+        #endregion
+    }
+}
