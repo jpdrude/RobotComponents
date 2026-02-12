059f90430b46587b363b18704554d4df783862d8
FilipHae <philipp.haenyes@web.de>
2026-02-12 17:01:57 +0100

    Fix event handler leak in 12 additional components
    Same bug as RobotToolComponent/WorkObjectComponent: doc.ObjectsDeleted
    was subscribed every solve without unsubscribing. Covers LoadData,
    SyncMoveOn/Off, TaskList, WaitSyncTask, ZoneData, ConfigurationData,
    SpeedData, JointTarget, ExternalJointPosition, RobotTarget, and
    RobotJointPosition components.

--- RobotComponents.ABB.Gh/Components/Code Generation/Declarations/ConfigurationDataComponent.cs
+++ RobotComponents.ABB.Gh/Components/Code Generation/Declarations/ConfigurationDataComponent.cs
@@ -168,6 +168,7 @@ namespace RobotComponents.ABB.Gh.Components.CodeGeneration
 
                 if (doc != null)
                 {
+                    doc.ObjectsDeleted -= this.DocumentObjectsDeleted;
                     doc.ObjectsDeleted += this.DocumentObjectsDeleted;
                 }
                 #endregion
--- RobotComponents.ABB.Gh/Components/Multi Move/WaitSyncTaskComponent.cs
+++ RobotComponents.ABB.Gh/Components/Multi Move/WaitSyncTaskComponent.cs
@@ -124,6 +124,7 @@ namespace RobotComponents.ABB.Gh.Components.MultiMove
 
                 if (doc != null)
                 {
+                    doc.ObjectsDeleted -= this.DocumentObjectsDeleted;
                     doc.ObjectsDeleted += this.DocumentObjectsDeleted;
                 }
                 #endregion
